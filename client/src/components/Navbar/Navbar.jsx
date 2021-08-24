de8a0d0ca143afb2a3ef9a9615b191bfe61b18cf
mnebot <miguelnbt@icloud.com>
2021-08-24 15:27:02 -0400

    grow and color on hover add to nav links

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className ="nav">
-      <Link to ='/'className ="links">Miguel</Link>
-      <Link to ='/about' className ="links">About</Link>
-      <Link to ='/projects' className ="links">Projects</Link>
-      <Link to ='/contact' className ="links">Contact</Link>
-      <Link to ={{pathname: 'https://docs.google.com/document/d/1yNrzNzwUCf9preQ6s3uSsZngi3RrOUnhGbmq33nrLgc/edit?usp=sharing'}} target="_blank" className ="links">Resumé</Link>
+      <Link to ='/'className ="links grow">Miguel</Link>
+      <Link to ='/about' className ="links grow">About</Link>
+      <Link to ='/projects' className ="links grow">Projects</Link>
+      <Link to ='/contact' className ="links grow">Contact</Link>
+      <Link to ={{pathname: 'https://docs.google.com/document/d/1yNrzNzwUCf9preQ6s3uSsZngi3RrOUnhGbmq33nrLgc/edit?usp=sharing'}} target="_blank" className ="links grow">Resumé</Link>
     </nav>
   );
 };
