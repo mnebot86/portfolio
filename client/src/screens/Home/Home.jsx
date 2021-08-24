90d9b32e071dd9d1358184187c3ffa0ae18c3c0e
mnebot <miguelnbt@icloud.com>
2021-08-24 16:44:48 -0400

    add title and words animation to home screen

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className="home-screen">
       <div className ="home-wrap">
         <div className="home-title">
-          <p>Software Engineer</p>
+          <p><span className="S">Software</span> <span className="E">Engineer</span></p>
         </div>
         <div className="home-words">
           <p>Creativity</p>
