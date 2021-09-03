143eecc53ba01e24c2f92fb922fbb41ee3def7c5
mnebot <miguelnbt@icloud.com>
2021-09-02 20:58:27 -0400

    gave top button % instead of px for better resizing

--- client/src/screens/Projects/Projects.jsx
+++ client/src/screens/Projects/Projects.jsx
@@ -1,35 +1,46 @@
 import "./Projects.css";
+import { useIntersectionObserver } from '../../useIntersectionObserve'
+import { useEffect,useRef } from "react";
 import mixer from "../../asset/projects/mixer.png";
 import arthub from "../../asset/projects/arthub.png";
 import gnome from "../../asset/projects/gnome.png"
 import barber from "../../asset/projects/barbershop.png";
 
 const Projects = () => {
+  const projectRef = useRef(null)
+  const [inView, entry] = useIntersectionObserver(projectRef, {
+    threshold:0
+  })
+
+  useEffect(() => {
+    console.log(inView);
+  }, [inView]);
+
   return (
     <div id='projects' className="projects-screen">
       <h1 className='projects-title'>Projects</h1>
-      <div className="mixer-card">
+      <div ref={projectRef} className={`mixer-card ${inView ?'project-fade' : ""}`}>
         <img src={mixer} alt="drinks" />
         <div className ="mixer-button-wrap">
           <button className ="projects-btn" type="button" onClick={() => window.open("https://ga-mixer.netlify.app/", "_blank")}>Live</button>
           <button className ="projects-btn" type="button" onClick={()=> window.open("https://github.com/mnebot86/mixer", "_blank")}>GitHub</button>
         </div>
       </div>
-      <div className="arthub-card">
+      <div ref={projectRef} className={`arthub-card ${inView ?'project-fade' : ""}`}>
         <img src={arthub} alt="gallery" />
         <div className ="arthub-button-wrap">
           <button className ="projects-btn" type="button" onClick={() => window.open("https://arthub-flamingos.netlify.app/", "_blank")}>Live</button>
           <button className ="projects-btn" type="button" onClick={() => window.open("https://github.com/mnebot86/arthub", "_blank")}>GitHub</button>
         </div>
       </div>
-      <div className="gnome-card">
+      <div ref={projectRef} className={`gnome-card ${inView ?'project-fade' : ""}`}>
         <img src={gnome} alt="shop" />
         <div className ="gnome-button-wrap">
           <button className ="projects-btn" type="button" onClick={() => window.open("https://gnome-a-zon.netlify.app/", "_blank")}>Live</button>
           <button className ="projects-btn" type="button" onClick={() => window.open("https://github.com/mnebot86/Gnome-A-zon", "_blank")}>GitHub</button>
         </div>
       </div>
-      <div className="barber-card">
+      <div ref={projectRef} className={`barber-card ${inView ?'project-fade' : ""}`}>
         <img src={barber} alt="haircut" />
         <div className ="barber-button-wrap">
           <button className ="projects-btn" type="button" onClick={() => window.open("https://thebarbershop401.netlify.app/", "_blank")}>Live</button>
